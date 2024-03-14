fd81f74dce07bfcc976203a4a8da416b13e79af9
Toty8 <80042351+Toty8@users.noreply.github.com>
2024-03-14 18:19:16 +0200

    Add css and html

--- src/app/cars/brand/brands-list/brands-list.component.ts
+++ src/app/cars/brand/brands-list/brands-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-brands-list',
   templateUrl: './brands-list.component.html',
   styleUrls: ['./brands-list.component.css']
 })
-export class BrandsListComponent {
+export class BrandsListComponent implements OnInit{
+
+  constructor(private api: ApiService){}
+
+  ngOnInit(): void {
+    this.api.getBrands().subscribe((brands) => {
+      console.log(brands);
+    });
+  }
 
 }
