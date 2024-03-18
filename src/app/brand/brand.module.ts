7fd6b8e462fbf8d19beb45357a47935977b0d1cb
Toty8 <80042351+Toty8@users.noreply.github.com>
2024-03-18 23:24:05 +0200

    Add 'add brand' component

--- src/app/brand/brand.module.ts
+++ src/app/brand/brand.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrandRoutingModule } from './brand-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { AddBrandComponent } from './add-brand/add-brand.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddBrandComponent
+  ],
   imports: [
     CommonModule,
     BrandRoutingModule,
