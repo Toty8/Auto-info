1e8e22d0802f3236e786392e6697c0edde69026c
Toty8 <80042351+Toty8@users.noreply.github.com>
2024-03-20 00:05:59 +0200

    Add fake login and logout logic

---
+++ src/app/types/user.ts
@@ -0,0 +1,11 @@
+export interface UserForAuth{
+    _id: string
+    email: string
+    username: string
+    password: string
+}
+
+export interface User{
+    _id: string
+    username: string
+}
--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(private userServise: UserService, private router: Router){}
 
+  login(ev:Event, email: string, password: string){
+    ev.preventDefault();
+    this.userServise.login();
+    this.router.navigate(['/']);
+  }
 }
