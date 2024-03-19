c7a904d0104451e838cb77118b7c060f483f2c06
Toty8 <80042351+Toty8@users.noreply.github.com>
2024-03-20 00:54:47 +0200

    Add create brand functionality

--- src/app/brand/add-brand/add-brand.component.ts
+++ src/app/brand/add-brand/add-brand.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-add-brand',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-brand.component.css']
 })
 export class AddBrandComponent {
+  
+  constructor(private apiService: ApiService){}
 
+  addBrand(ev: Event, brandName: string, brandImageUrl: string){
+    ev.preventDefault();
+    
+    this.apiService.createBrand(brandName, brandImageUrl).subscribe();
+  }
 }
