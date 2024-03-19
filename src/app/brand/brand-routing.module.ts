ec84682ab59cf3931b066a07a26fa81249bf51fc
Toty8 <80042351+Toty8@users.noreply.github.com>
2024-03-20 00:12:05 +0200

    Add route guard and fix paths

--- src/app/brand/brand-routing.module.ts
+++ src/app/brand/brand-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { BrandsListComponent } from "./brands-list/brands-list.component";
 import { AddBrandComponent } from "./add-brand/add-brand.component";
+import { AuthActivate } from "../guards/auth.activate";
+import { ModelsListComponent } from "../model/models-list/models-list.component";
 
 const routes: Routes = [
-    {path: 'brand', children: [
+    {path: 'brands', children: [
         {path: '', pathMatch: 'full', component: BrandsListComponent},
-        {path: 'add-brand', component: AddBrandComponent},
+        {path: 'add-brand', component: AddBrandComponent, canActivate: [AuthActivate]},
+        {path: ':brandId', component: ModelsListComponent}
     ]}
 ];
 
