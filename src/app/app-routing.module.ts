c2f0a9b2bf77a381ab5dfe87ccab15eda7a97918
Toty8 <80042351+Toty8@users.noreply.github.com>
2024-03-20 00:54:01 +0200

    Fix routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: '/brand'},
+  {path: '', pathMatch: 'full', redirectTo: '/brands'},
   {path: '**', redirectTo: '/404'},
   {path: '404', component: ErrorComponent},
 ];
