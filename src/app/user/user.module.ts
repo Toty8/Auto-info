03e725f37a1769d3a12ee7be40142d3e6b79b5e2
Toty8 <80042351+Toty8@users.noreply.github.com>
2024-03-26 12:28:30 +0200

    Add reactive driven form validation for register

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserRoutingModule } from './user-routing.module';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -13,7 +16,11 @@ import { UserRoutingModule } from './user-routing.module';
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    RouterModule,
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class UserModule { }
