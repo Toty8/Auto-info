ffca1ca728e48391f68e7ca6d3d240348eecf638
Toty8 <80042351+Toty8@users.noreply.github.com>
2024-04-01 20:47:14 +0300

    Make error page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ErrorComponent } from './error/error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ErrorComponent } from './core/error/error.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: '/brands'},
+  {path: 'error', component: ErrorComponent},
   {path: '**', redirectTo: '/404'},
-  {path: '404', component: ErrorComponent},
+  {path: '404', component: NotFoundComponent},
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
