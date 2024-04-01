cea07e4d30063f65d134f04a5609471dcdb3098e
Toty8 <80042351+Toty8@users.noreply.github.com>
2024-04-02 00:02:45 +0300

    Make add-model routing and fix buttons style

--- src/app/model/model-routing.module.ts
+++ src/app/model/model-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AddModelComponent } from "./add-model/add-model.component";
+import { AuthActivate } from "../guards/auth.activate";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: 'models', children: [
+        {path: ':brandId/add-model', canActivate: [AuthActivate], component: AddModelComponent}
+    ]}
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
